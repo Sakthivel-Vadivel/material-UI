7e5afc322468e82e08cc838d2edc89a84c66a78a
Sakthivel-Vadivel <139963836+Sakthivel-Vadivel@users.noreply.github.com>
2023-07-19 15:12:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import MUIButton from "./MUIButton";
+//import "./App.css";
+//import Button from "@mui/material/Button";
+
+function App() {
+  return (
+    <div>
+      {<MUIButton />}
+      <h2>Helooo</h2>
+    </div>
+  );
+}
+
+export default App;
