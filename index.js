e5e91a1593439eb5024982e997f8eea2fb0ad555
Sakthivel-Vadivel <sakthivellvadivel@gmail.com>
2023-07-20 10:08:41 +0530

    feat: add modify option

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ console.log("hello world with conventional commit");
 console.log("checking the version with feature");
 
 console.log("final conv check");
+
+console.log(" checking modified iption ");
